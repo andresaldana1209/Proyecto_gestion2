dc719703254132d02234163704ab9e2dd97fa9f9
andresaldana1209 <61165319+andresaldana1209@users.noreply.github.com>
2022-03-09 10:58:51 -0500

    commit 1.2

--- js/programa.js
+++ js/programa.js
@@ -2,12 +2,18 @@ function cambiarColor(){
     var x = document.getElementById("titulo");
     x.style.color = 'red';
     x.style.backgroundColor = 'yellow';
-
 }
 
 function RestablecerColor(){
     var x = document.getElementById("titulo");
     x.style.color = 'black';
     x.style.backgroundColor = 'aqua';
+}
 
+function obtenerPareseImpares(){
+    var numeros = document.getElementById("Numero4D");
+    
+    if (numeros % 2 == 0){
+        alert("numero par")
+    }
 }
