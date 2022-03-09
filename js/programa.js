4d29b8917111fabcac9a305ca074277f6569dab1
andresaldana1209 <61165319+andresaldana1209@users.noreply.github.com>
2022-03-09 10:52:29 -0500

    Commit 1.1

--- js/programa.js
+++ js/programa.js
@@ -1,1 +1,13 @@
+function cambiarColor(){
+    var x = document.getElementById("titulo");
+    x.style.color = 'red';
+    x.style.backgroundColor = 'yellow';
 
+}
+
+function RestablecerColor(){
+    var x = document.getElementById("titulo");
+    x.style.color = 'black';
+    x.style.backgroundColor = 'aqua';
+
+}
